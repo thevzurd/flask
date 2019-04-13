846bc3f4efd2845e6094edc6de97b0eba23ce4ed
thevzurd <thevzurd@gmail.com>
2019-04-13 13:47:11 -0400

    some changes

--- js_example/views.py
+++ js_example/views.py
@@ -2,15 +2,22 @@ from flask import jsonify, render_template, request
 
 from js_example import app
 
+@app.route("/home")
+def home():
+    return render_template("home.html")
 
-@app.route('/', defaults={'js': 'plain'})
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+''' @app.route('/', defaults={'js': 'plain'})
 @app.route('/<any(plain, jquery, fetch):js>')
 def index(js):
-    return render_template('{0}.html'.format(js), js=js)
-
+    return render_template('{0}.html'.format(js), js=js) '''
+''' 
 
 @app.route('/add', methods=['POST'])
 def add():
     a = request.form.get('a', 0, type=float)
     b = request.form.get('b', 0, type=float)
-    return jsonify(result=a + b)
+    return jsonify(result=a + b) '''
