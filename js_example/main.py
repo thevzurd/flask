07d6fe1c85da339fedd9e8bbdffc993d59ac84aa
alend-concordia <49287109+alend-concordia@users.noreply.github.com>
2019-04-17 15:22:09 -0400

    added vienna libraries

--- js_example/main.py
+++ js_example/main.py
@@ -1,19 +1,20 @@
 import random
-import converter as cv
+from js_example import converter as cv
 import json
 
-def main(inp,out):
-    bases = ['A','C','U','G']
-    if not inp :
-        l = random.randint(10,20)
+
+def main(inp, out):
+    bases = ['A', 'C', 'U', 'G']
+    if not inp:
+        l = random.randint(10, 20)
         inp = [random.choice(bases) for _ in range(l)]
 
-    if not out :
-        l = random.randint(10,20)
+    if not out:
+        l = random.randint(10, 20)
         out = [random.choice(bases) for _ in range(l)]
 
-    _, seq, constrain, conv_output,fold1,fold2,ed1,ed2,mfe1,mfe2 = \
-                                                    cv.convgen(inp,out)
+    _, seq, constrain, conv_output, fold1, fold2, ed1, ed2, mfe1, mfe2 = \
+        cv.convgen(inp, out)
 
     data = {}
     data['Sequence'] = seq
@@ -29,14 +30,13 @@ def main(inp,out):
     with open('output.json', 'w') as f:
         json.dump(data, f)
 
-
-    print '>1'
-    print seq
-    print fold1
-    print '>2'
-    print seq
-    print fold2
+    print('>1')
+    print(seq)
+    print(fold1)
+    print('>2')
+    print(seq)
+    print(fold2)
 
 
 if __name__ == '__main__':
-    main("","")
+    main("", "")
--- js_example/views.py
+++ js_example/views.py
@@ -1,9 +1,13 @@
 from flask import jsonify, render_template, request, url_for
 
 from js_example import app
+from js_example import converter as cv
+import random
+import json
 
 @app.route('/')
 def home():
+    main("", "")
     return render_template('home.html')
 
 @app.route('/output')
@@ -21,3 +25,36 @@ def add():
     a = request.form.get('a', 0, type=float)
     b = request.form.get('b', 0, type=float)
     return jsonify(result=a + b) '''
+def main(inp, out):
+    bases = ['A', 'C', 'U', 'G']
+    if not inp:
+        l = random.randint(10, 20)
+        inp = [random.choice(bases) for _ in range(l)]
+
+    if not out:
+        l = random.randint(10, 20)
+        out = [random.choice(bases) for _ in range(l)]
+
+    _, seq, constrain, conv_output, fold1, fold2, ed1, ed2, mfe1, mfe2 = \
+        cv.convgen(inp, out)
+
+    data = {}
+    data['Sequence'] = seq
+    data['Inactive'] = fold1
+    data['Inactive MFE'] = mfe1
+    data['Inactive ED'] = ed1
+    data['Active'] = fold2
+    data['Active MFE'] = mfe2
+    data['Active ED'] = ed2
+    data['Input'] = "".join(inp)
+    data['Output'] = "".join(out)
+
+    with open('output.json', 'w') as f:
+        json.dump(data, f)
+
+    print('>1')
+    print(seq)
+    print(fold1)
+    print('>2')
+    print(seq)
+    print(fold2)
