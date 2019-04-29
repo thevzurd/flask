39964c72dd52d25d350d411d329904877ff2a998
thevzurd <thevzurd@gmail.com>
2019-04-29 00:13:48 -0400

    final version 1.0

--- js_example/__init__.py
+++ js_example/__init__.py
@@ -1,12 +1,10 @@
 # https://medium.freecodecamp.org/how-to-build-a-web-application-using-flask-and-deploy-it-to-the-cloud-3551c985e492
 # https://stackoverflow.com/questions/18713086/virtualenv-wont-activate-on-windows
 
-##from flask_scss import Scss
 from flask import Flask
 
 app = Flask(__name__)
 
-##Scss(app, static_dir='static', asset_dir='assets')
 
 from js_example import views
 if __name__ == "__main__":
